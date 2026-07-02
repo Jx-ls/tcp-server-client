c639c10db6254d82ebc51f1b24011832f0af97ab
Jx-ls <joshuarajtadi@gmail.com>
2026-07-03 02:50:01 +0530

    tcp server

---
+++ server.cpp
@@ -0,0 +1,35 @@
+#include "utils.h"
+
+int main (int argc, char **argv) 
+{
+    int                 listenfd, connfd;
+    struct sockaddr_in  servaddr{};
+    char                buff[MAXLINE];
+    time_t              ticks;
+
+    if (argc != 2)
+        cerr << "Usage: server <PORT>\n", exit(0);
+
+    listenfd = Socket(AF_INET, SOCK_STREAM, 0);
+
+    servaddr.sin_family = AF_INET;
+    servaddr.sin_addr.s_addr = htonl(INADDR_ANY);
+    servaddr.sin_port = htons(atoi(argv[1]));
+
+    Bind(listenfd, (struct sockaddr *) &servaddr, sizeof(servaddr));
+
+    Listen(listenfd, MAX_CONN);
+
+    for ( ; ; ) {
+        connfd = Accept(listenfd, (struct sockaddr *) NULL, NULL);
+        cout << "Connected\n";
+
+        ticks = time(NULL);
+        snprintf(buff, sizeof(buff), "%.24s\r\n", ctime(&ticks));
+
+        Write(connfd, buff, strlen(buff));
+
+        Close(connfd);
+    }
+}
+
