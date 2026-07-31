0a03a18d8b2c0f09086b96c90d5e736362dcc223
Jx-ls <joshuarajtadi@gmail.com>
2026-08-01 01:50:50 +0530

    update: client to test rate limiting

--- src/client.cpp
+++ src/client.cpp
@@ -1,25 +1,56 @@
 #include "../include/utils.h"
+#include "../include/server_core.h"
+#include <string>
 
-int main (int argc, char **argv) 
-{
-    int                 sockfd, n;
-    struct sockaddr_in  servaddr{};
-    char                recvline[MAXLINE + 1];
+void send_message(int sockfd, const std::string& payload) {
+    MessageHeader hdr;
+    hdr.payload_length = payload.size();
+    hdr.msg_type = 1;
 
+    std::vector<uint8_t> buffer(sizeof(MessageHeader) + payload.size());
+    memcpy(buffer.data(), &hdr, sizeof(MessageHeader));
+    memcpy(buffer.data() + sizeof(MessageHeader), payload.data(), payload.size());
+
+    Write(sockfd, buffer.data(), buffer.size());
+}
+
+int main(int argc, char **argv) {
     if (argc != 3)
         cerr << "Usage: client <IP_ADRESS> <PORT>\n", exit(EXIT_FAILURE);
     
-    sockfd = Socket(AF_INET, SOCK_STREAM, 0);
+    int sockfd = Socket(AF_INET, SOCK_STREAM, 0);
 
+    struct sockaddr_in servaddr{};
     servaddr.sin_family = AF_INET;
     servaddr.sin_port = htons(atoi(argv[2]));
-    if ( inet_pton(AF_INET, argv[1], &servaddr.sin_addr) <= 0)
+    if (inet_pton(AF_INET, argv[1], &servaddr.sin_addr) <= 0)
         cerr << "Invalid IP: " << strerror(errno) << "\n", exit(EXIT_FAILURE);
 
     Connect(sockfd, (struct sockaddr *) &servaddr, sizeof(servaddr));
+    cout << "Connected to server. Sending requests...\n";
 
-    n = Read(sockfd, recvline, MAXLINE);
-    recvline[n] = 0;
-    cout << recvline;
+    // Send 15 messages quickly to trigger the rate limiter (burst is 10)
+    for (int i = 0; i < 15; i++) {
+        send_message(sockfd, "Hello Server, message #" + to_string(i));
+    }
+
+    // Read responses
+    char recvline[4096];
+    while (true) {
+        ssize_t n = read(sockfd, recvline, sizeof(recvline));
+        if (n > 0) {
+            // Simplified parsing: Assume we read the header and payload together for the demo
+            if (n >= sizeof(MessageHeader)) {
+                MessageHeader* hdr = (MessageHeader*)recvline;
+                std::string payload(recvline + sizeof(MessageHeader), hdr->payload_length);
+                cout << "Received: " << payload << "\n";
+            }
+        } else if (n == 0) {
+            cout << "Server closed connection.\n";
+            break;
+        }
+    }
+    
+    Close(sockfd);
     return 0;
 }
